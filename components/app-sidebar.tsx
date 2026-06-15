a923464dbf1f18fa7b3ea956499707c82c70c76c
harshaggarwal04 <harshaggarwal0445@gmail.com>
2026-06-15 21:36:00 +0530

    Dashboard Page Backend

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,6 +14,7 @@ import Logout from '@/modules/auth/components/logout'
 
 
 export const AppSidebar = () => {
+    
 
     const { theme, setTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
