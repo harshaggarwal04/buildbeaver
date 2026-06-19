69dadf5fff3d1bb8b5658d034ae7dba6faa08fb9
harshaggarwal04 <harshaggarwal0445@gmail.com>
2026-06-19 11:46:20 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: true
+  cacheComponents: true,
+  serverExternalPackages: ["inngest"],
+
 };
 
 export default nextConfig;
